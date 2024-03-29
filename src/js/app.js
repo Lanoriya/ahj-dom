9e6dd5e751f6f0444654454ac8a0a70fd1e227de
lanoriya <Mr_Buzik@mail.ru>
2024-03-29 18:20:59 +0500

    Add files

---
+++ src/js/app.js
@@ -0,0 +1,18 @@
+import Icon from './goblin.png';
+
+let cells = document.querySelectorAll('.cell')
+let img = document.createElement('img');
+
+img.src = Icon
+img.classList.add('goblin')
+
+let prevCellIndex = -1;
+setInterval(() => {
+  let cellsRandom;
+  do {
+    cellsRandom = Math.floor(Math.random() * cells.length);
+  } while (cellsRandom === prevCellIndex);
+  
+  prevCellIndex = cellsRandom;
+  cells[cellsRandom].appendChild(img)
+}, 1000);
